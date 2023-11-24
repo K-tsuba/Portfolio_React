9c40f71e77b8883fff15220ce0d7775ebab712bf
K-tsuba <t.ko-he-1999.622.fifteen@docomo.ne.jp>
2023-11-24 19:41:54 +0900

    CONTACT 修正

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Navbar, Nav } from 'react-bootstrap';
-import { Link } from 'react-scroll';
-import { useLocation } from 'react-router-dom';
+// import { Link } from 'react-scroll';
+// import { useLocation } from 'react-router-dom';
 
 const Header = () => {
 
-  const location = useLocation();
+  // const location = useLocation();
 
   return <header className='sticky-top'>
     <Navbar collapseOnSelect expand="lg" className='px-3'>
@@ -21,9 +21,9 @@ const Header = () => {
           <Nav.Link href="/skills">SKILLS</Nav.Link>
           <Nav.Link href="/works">WORKS</Nav.Link>
           <Nav.Link href="/contact">CONTACT</Nav.Link>
-          {location.pathname === '/' && (
+          {/* {location.pathname === '/' && (
           <Link to="contact" smooth={true} duration={10} className='contact'>CONTACT</Link>
-          )}
+          )} */}
         </Nav>
       </Navbar.Collapse>
     </Navbar >
