41fa37e37c039152b860714ac61b36a70626c102
K-tsuba <t.ko-he-1999.622.fifteen@docomo.ne.jp>
2023-11-25 18:53:23 +0900

    お問い合わせページの修正

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,8 +10,17 @@ const Contact = () => {
       <div className="container">
         <div className='custom-container'>
           <Header />
-          <div className='py-5 form-width'>
-            
+          <div className='row pt-5 pb-4'>
+            <div className="col-12 text-center mb-3">
+              <h3 className='mt-4'>CONTACT</h3>
+              <div className='title-border'></div>
+            </div>
+          </div>
+          <div className='form-width pb-5'>
+            <div className='pb-4'>
+              <p className=''>Web関係の仕事依頼を承ります。</p>
+              <p>I am open to accepting business inquiries for web-related projects.</p>
+            </div>
             <form name="contact" method="POST" data-netlify="true" action="/dummy_thanks.html">
               <div class="mb-3">
                 <label for="name" class="form-label">Name</label>
@@ -26,9 +35,8 @@ const Contact = () => {
                 <textarea class="form-control" id="message" name="message" rows="4" required></textarea>
               </div>
               <input type="hidden" name="form-name" value="contact" />
-              <button type="submit" class="btn btn-primary">Submit</button>
+              <button type="submit" class="btn btn-secondary">Submit</button>
             </form>
-
           </div>
         </div>
       </div>
